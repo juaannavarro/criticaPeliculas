f54e70a068eeafb03e3ed90d94642f4f463d1c23
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-04-27 19:55:49 +0200

    csv

--- clases/_pycache_/calculo.py
+++ clases/_pycache_/calculo.py
@@ -1,10 +1,7 @@
 from collections import Counter
 from turtle import pd
-import matplotlib.pyplot as plt
-from colorama import Fore, Style
-import math
 
-datos = pd.read_csv("datosCriticapelicula.csv", sep =";")
+
 class Calculo:
     def __init__(self, calcular):
         self.calcular = calcular
