6c6ef28845411fe29a145e9a3e05e4b39a10376b
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-04-27 23:41:34 +0200

    fin

--- calculo.py
+++ calculo.py
@@ -1,5 +1,6 @@
 import csv
-
+import numpy as np
+import math
 lista1 = []
 lista2 = []
 with open(file="datosCriticapelicula.csv", mode="r") as f:
@@ -15,7 +16,7 @@ print("Columna 2: ",lista2)
 
 
 
-resultado= int(lista1[1])+int(lista2[3])
+resultado= int(lista1[3])*int(lista2[3])
 print("Resultado: ", resultado)
 
 
@@ -26,84 +27,19 @@ print("Resultado: ", resultado)
 
 
 from collections import Counter
-from math import *
 import matplotlib.pyplot as plt
 class Estadistica:
     
         def __init__(self, caracteristica):
             self.caracteristica = caracteristica
-    
-        def calculoMediaAritmetica(self):
-            n = self.caracteristica.count()
-            sumaValoresObservaciones = 0
-            mediaAritmetica = 0
-            for valorObservacion in self.caracteristica:
-                sumaValoresObservaciones = sumaValoresObservaciones + valorObservacion
-    
-            mediaAritmetica = sumaValoresObservaciones / n
-            return mediaAritmetica
-    
-        def calculoMediana(self):
-            mediana = 0
-            caracteristica = self.caracteristica.sort_values()
-            caracteristica = caracteristica.reset_index(drop=True)
-            n = self.caracteristica.count()
-            par = False;
-            if (n % 2 == 0):
-                print("La cantidad de observaciones es par.")
-                par = True
-    
-            if par:
-                rango = (n / 2);
-                print("RANGO = " + str(rango))
-                rangoPython = rango - 1
-                valor1 = caracteristica[rangoPython]
-                valor2 = caracteristica[rangoPython + 1]
-                mediana = valor1 + ((valor2 - valor1) / 2)
-            else:
-                rango = ((n + 1) / 2)
-                rangoPython = rango - 1
-                mediana = caracteristica[rangoPython]
-    
-            return [mediana, rango]
-    
-        def calculoModa(self):
-            moda = Counter(self.caracteristica)
-            return moda
-    
-        def calculoVarianzaDesviacionTipica(self):
-            n = self.caracteristica.count()
-            mediaAritmetica = self.caracteristica.mean()
-            varianza = 0
-            c3 = 0
-            for valorObservacion in self.caracteristica:
-                x = valorObservacion
-                moy = mediaAritmetica
-                c1 = valorObservacion - mediaAritmetica
-                c2 = c1 ** 2
-                c3 = c3 + c2
-            varianza = c3 / n
-            desviacionTipica = sqrt(varianza)
-            return [varianza, desviacionTipica]
-        def percentiles(self, percentiles):
-            caracteristica = self.caracteristica.sort_values()
-            caracteristica = caracteristica.reset_index(drop=True)
-            n = self.caracteristica.count()
-            par = False;
-            if (n % 2 == 0):
-                print("La cantidad de observaciones es par.")
-                par = True
-    
-            if par:
-                rango = (n / 2);
-                print("RANGO = " + str(rango))
-                rangoPython = rango - 1
-                valor1 = caracteristica[rangoPython]
-                valor2 = caracteristica[rangoPython + 1]
-                mediana = valor1 + ((valor2 - valor1) / 2)
-            else:
-                rango = ((n + 1) / 2)
-                rangoPython = rango - 1
-                mediana = caracteristica[rangoPython]
-    
-            return [mediana, rango]
+            self.cantidad_valoraciones = np.array([42, 96, 132, 124, 88, 58])
+            self.cantidad_valoraciones_ordenadas = np.sort(self.cantidad_valoraciones)
+            self.cantidad_valoraciones_ordenadas_invertidas = np.sort(self.cantidad_valoraciones)[::-1]
+            self.cantidad_valoraciones_ordenadas_invertidas_con_nan = np.sort(self.cantidad_valoraciones)[::-1]
+
+
+        def percentiles(cantidad_valoraciones_ordenadas_invertidas_con_nan):
+            cantidad_valoraciones = np.array([42, 96, 132, 124, 88, 58])
+            p = np.percentile(cantidad_valoraciones, [68, 95, 97])
+            return p
+print(Estadistica.percentiles("cantidad_valoraciones_ordenadas_invertidas_con_nan"))
